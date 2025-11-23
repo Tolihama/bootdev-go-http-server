86f28096e55e8fbc903079da4d35e1f76c88ca97
Francesco Marino <fra070@gmail.com>
2025-11-23 12:09:42 +0100

    set health endpoint

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"io"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./server")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./server"))))
+	mux.HandleFunc("/healthz", healthz)
 	server := http.Server{}
 	server.Addr = ":8080"
 	server.Handler = mux
 	server.ListenAndServe()
+}
+
+func healthz(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
 }
